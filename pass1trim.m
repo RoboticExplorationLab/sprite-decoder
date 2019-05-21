00fceef5e8fb6a36f8498120f1879558d244a4ae
Zac Manchester <zacmanchester@stanford.edu>
2019-05-20 21:25:51 -0700

    added missing filter bandwidth

--- pass1trim.m
+++ pass1trim.m
@@ -6,7 +6,7 @@ raw = read_complex_binary_chunk('kicksat2_20190320_032746_436935500_1800000_fc.r
 Fc = 315e3/900e3;
 
 %Prototype low-pass filter
-[~,lpf] = lowpass(raw(1:18000), ,1800e3,'Steepness',0.97);
+[~,lpf] = lowpass(raw(1:18000),80e3,1800e3,'Steepness',0.97);
 
 %Create bandpass filter by frequency shifting taps up to Fc
 N = length(lpf.Coefficients)-1;
